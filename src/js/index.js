6c0b3d556346da698cc9044c1ce99d478ab3824a
SamZakharov <semen.zaharovv@gmail.com>
2024-01-22 21:59:32 +0200

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line func-names
+const sum = function (a, b) {
+  return a + b;
+};
+
+sum(1, 2);
+
+// eslint-disable-next-line func-names
+const minus = function (a, b) {
+  return a - b;
+};
+
+minus(1, 2);
+
+// eslint-disable-next-line func-names
+const mul = function (a, b) {
+  return a * b;
+};
+
+mul(1, 2);
